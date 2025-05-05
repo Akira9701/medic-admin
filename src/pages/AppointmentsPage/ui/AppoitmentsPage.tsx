8583313dc4e4fa461fa824e00d6959b69971b159
Akira <antonov.egor@yandex.ru>
2025-05-06 03:43:36 +0700

    feat: fix appointments

--- src/pages/AppointmentsPage/ui/AppoitmentsPage.tsx
+++ src/pages/AppointmentsPage/ui/AppoitmentsPage.tsx
@@ -3,12 +3,11 @@ import useAppointmentsStore, {
   setAppointments,
 } from '@/entities/Appointments/model/appointments.store';
 import apointmentsApi from '@/entities/Appointments/api';
-import { useNavigate } from 'react-router';
 import useUserStore from '@/entities/User/model/user.store';
 import Appointment from './components/Appointment';
+import { LoaderPinwheel } from 'lucide-react';
 
 const AppointmentsPage = () => {
-  const navigate = useNavigate();
   const appointments = useAppointmentsStore((state) => state.appointments);
   const vetId = useUserStore((state) => state.user?.id);
 
@@ -20,14 +19,18 @@ const AppointmentsPage = () => {
   }, []);
 
   return (
-    <div>
+    <div className="w-full h-full relative">
       <h2 className="mt-10 mb-3 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
         Appointments
       </h2>
-      <div className="grid grid-cols-3 gap-4">
-        {appointments.map((appointment) => (
-          <Appointment appointment={appointment} />
-        ))}
+      <div className="grid grid-cols-3 gap-4 w-full h-full">
+        {appointments.length > 0 ? (
+          appointments.map((appointment) => <Appointment appointment={appointment} />)
+        ) : (
+          <div className="absolute h-fit w-fit inset-0 m-auto">
+            <LoaderPinwheel className="animate-spin" />
+          </div>
+        )}
       </div>
     </div>
   );
